de7f9e2c750a0cef3000d5a12bbcdc5722515a36
darkdeymon <darkdeymon04@gmail.com>
2014-09-10 00:16:29 -0400

    Version 3

--- unidad_democratica/apps/usuarios/admin.py
+++ unidad_democratica/apps/usuarios/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from models import control_electoral
+
 # Register your models here.
 class control_electoral_vista(admin.ModelAdmin):
-	
 	list_display = ('nombre','apellido_paterno','apellido_materno','ci','sircunscripcion','municipio','recinto','mesa','ci','fecha')
+
 admin.site.register(control_electoral,control_electoral_vista)
--- unidad_democratica/apps/usuarios/models.py
+++ unidad_democratica/apps/usuarios/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+# Create your models here
 
-# Create your models here.
 class control_electoral(models.Model):
-	nombre=models.CharField(max_length=100)
-	apellido_paterno=models.CharField(max_length=100)
-	apellido_materno=models.CharField(max_length=100)
-	fecha=models.DateField(auto_now=True)
-	ci=models.IntegerField()
-	sircunscripcion=models.CharField(max_length=100)
-	municipio=models.CharField(max_length=100)
-	recinto=models.CharField(max_length=100)
-	mesa=models.CharField(max_length=100)
+	nombre = models.CharField(max_length=100)
+	apellido_paterno = models.CharField(max_length=100)
+	apellido_materno = models.CharField(max_length=100)
+	fecha = models.DateField(auto_now=True)
+	ci = models.IntegerField()
+	sircunscripcion = models.CharField(max_length=100)
+	municipio = models.CharField(max_length=100)
+	recinto = models.CharField(max_length=100)
+	mesa = models.CharField(max_length=100)
+	class Meta:
+		verbose_name = _('Control Electoral')
+		verbose_name_plural = _('Control Electoral')
